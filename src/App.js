adece8c1de66465ca45e4c4f8597e27b9cd3c223
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-08-04 18:10:07 -0400

    Working gif api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import './App.css';
+import { useState } from 'react'
 //import NavBar
 function App() {
+  const [input, setInput] = useState();
+  const [variable, setVariable] = useState();
+  const key = 'fxGxrDaQt4YRxPcnwVmUfx5gGILgddK7';
+  
+
+  function onChangeInput(e) {
+    // console.log(e.target.value);
+    setInput(e.target.value);
+  }
+  function handleForm(e) {
+    e.preventDefault();
+    console.log(input)
+    const url = `https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${input}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(newResponse => newResponse.data.images.original.url)
+      .then(result => setVariable(result))
+
+  }
+
   return (
     <div className="App">
       <div className="Header">
@@ -9,11 +31,19 @@ function App() {
         </h1>
       </div>
 
-      <form className="Search-Form">
-        <input type="text" id="Search-Text" placeholder="Search Location"/>
-        <button type="submit" id="Search-Btn">🔍</button>
-      </form>
-    </div>
+      <div className="Form-container">
+        <form className="Search-Form" onSubmit={handleForm}>
+          <input onChange={onChangeInput} type="text" id="Search-Text" placeholder="Search Location" />
+          <button type="submit" id="Search-Btn" >🔍</button>
+        </form>
+
+        <img src={variable} id="GIF-img" />
+      </div>
+
+      <div className="Weather-info">
+
+      </div>
+    </div >
   );
 }
 
