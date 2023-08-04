9ad9f3f89747ae75640fd2fe7de4e8a6de490e26
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-08-04 14:18:10 -0400

    Added header and form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+//import NavBar
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <h1 id="Header-Text">
+          Weather App
+        </h1>
+      </div>
+
+      <form className="Search-Form">
+        <input type="text" id="Search-Text" placeholder="Search Location"/>
+        <button type="submit" id="Search-Btn">🔍</button>
+      </form>
     </div>
   );
 }
